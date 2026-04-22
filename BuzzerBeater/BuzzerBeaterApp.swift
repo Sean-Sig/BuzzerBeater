9acd06c25fbccc54df51679615223af63ca1a4d0
Sean Siggard <sean@bleachr.co>
2026-04-22 11:31:52 -0400

    Initial Commit

---
+++ BuzzerBeater/BuzzerBeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzerBeaterApp.swift
+//  BuzzerBeater
+//
+//  Created by Sean Siggard on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct BuzzerBeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
